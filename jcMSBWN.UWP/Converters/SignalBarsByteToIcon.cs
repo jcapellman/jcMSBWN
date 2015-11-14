c4fc8c79daec45cc8ae9dfd8837c8d53c70a779b
Jarred Capellman <jcapellman@hotmail.com>
2015-11-13 23:17:46 -0500

    Added WiFi Signal Strength Icons

---
+++ jcMSBWN.UWP/Converters/SignalBarsByteToIcon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace jcMSBWN.UWP.Converters {
+    public class SignalBarsByteToIcon : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            var numBars = System.Convert.ToInt32(value);
+
+            return new BitmapImage(new System.Uri($"Assets/wifi_signal_{numBars}_bar.png", UriKind.RelativeOrAbsolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
